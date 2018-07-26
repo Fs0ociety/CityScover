f9daad42ca25a06300955ac323114271b1836a0a
Andrea Ritondale <andrea.ritondale@student.unife.it>
2018-07-26 17:13:55 +0200

    Creazione dei progetti:
    CityScover.Data
    CityScover.Engine
    CityScover.ADT.Graphs
    CityScover.Services
    CityScover.Tests
    CityScover

---
+++ CityScover.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CityScover.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CityScover.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2f577b29-4000-4f06-927b-7edd072b929e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
