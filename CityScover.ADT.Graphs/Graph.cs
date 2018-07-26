526daf4c88dc3d36ffb0ebf526e07175c589cf6b
Andrea Ritondale <andrea.ritondale@student.unife.it>
2018-07-26 18:06:41 +0200

    Inizio implementazione della libreria CityScover.ADT.Graphs

---
+++ CityScover.ADT.Graphs/Graph.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CityScover.ADT.Graphs
+{
+   public interface IGraphEdgeWeight
+   {
+      Func<double> Weight { get; }
+   }
+
+
+   public abstract class Graph<TNodeKey, TNodeData, TEdgeWeight>
+      where TNodeKey : struct
+      where TEdgeWeight : IGraphEdgeWeight
+   {
+      private readonly Dictionary<TNodeKey, GraphNode> _nodes;
+
+      #region Constructors
+      protected Graph()
+      {
+         _nodes = new Dictionary<TNodeKey, GraphNode>();
+      }
+      #endregion
+
+      #region Public properties
+      /// <summary>
+      /// Restituisce il numero di nodi.
+      /// </summary>
+      public int NodeCount => _nodes.Count;
+
+      /// <summary>
+      /// Restituisce il numero di archi.
+      /// </summary>
+      public int EdgeCount => _nodes.Sum(x => x.Value.Edges.Count());
+
+      /// <summary>
+      /// Restituisce l'insieme dei nodi.
+      /// </summary>
+      public IEnumerable<TNodeData> Nodes => _nodes.Values.Select(x => x.Data);
+
+      /// <summary>
+      /// Restituisce il dato del nodo specificato dalla chiave.
+      /// </summary>
+      /// <param name="key">Chiave del nodo.</param>
+      /// <returns>Dato del nodo specificato.</returns>
+      public TNodeData this[TNodeKey key] => _nodes[key].Data;
+      #endregion
+
+      #region Public methods
+      /// <summary>
+      /// Aggiunge un nuovo nodo identificato da una chiave e da un dato.
+      /// </summary>
+      /// <param name="key">Chiave.</param>
+      /// <param name="nodeData">Dato.</param>
+      /// <exception cref="InvalidOperationException"/>
+      public void AddNode(TNodeKey key, TNodeData nodeData)
+      {
+         // Non è possibile inserire un nuovo nodo con la stessa chiave
+         if (_nodes.ContainsKey(key))
+         {
+            throw new InvalidOperationException();
+         }
+         _nodes.Add(key, new GraphNode(key, nodeData));
+      }
+
+      /// <summary>
+      /// Aggiunge un nuovo arco orientato non pesato tra due nodi specificati.
+      /// </summary>
+      /// <param name="startNodeKey"></param>
+      /// <param name="endNodeKey"></param>
+      public void AddEdge(TNodeKey startNodeKey, TNodeKey endNodeKey)
+      {
+         // Non è possibile inserire un arco tra due nodi non esistenti
+         // oppure un arco con lo stesso nodo destinazione già esistente.
+         if (!_nodes.ContainsKey(startNodeKey) ||
+            !_nodes.ContainsKey(endNodeKey) ||
+            _nodes[startNodeKey].Edges.Where(x => Equals(x.DestNode.Key, endNodeKey)).Any())
+         {
+            throw new InvalidOperationException();
+         }
+         _nodes[startNodeKey].AddEdge(_nodes[endNodeKey]);
+      }
+
+      /// <summary>
+      /// Aggiunge un nuovo arco orientato e pesato tra due nodi specificati.
+      /// </summary>
+      /// <param name="startNodeKey">Chiave del nodo di partenza.</param>
+      /// <param name="endNodeKey">Chiave del nodo di arrivo.</param>
+      /// <param name="edgeWeight">Peso attribuito.</param>
+      /// <exception cref="InvalidOperationException"/>
+      public void AddEdge(TNodeKey startNodeKey, TNodeKey endNodeKey, TEdgeWeight edgeWeight)
+      {
+         // Non è possibile inserire un arco tra due nodi non esistenti
+         // oppure un arco con lo stesso nodo destinazione già esistente.
+         if (!_nodes.ContainsKey(startNodeKey) ||
+            !_nodes.ContainsKey(endNodeKey) ||
+            _nodes[startNodeKey].Edges.Where(x => Equals(x.DestNode.Key, endNodeKey)).Any())
+         {
+            throw new InvalidOperationException();
+         }
+         _nodes[startNodeKey].AddEdge(_nodes[endNodeKey], edgeWeight);
+      }
+
+      /// <summary>
+      /// Aggiunge un nuovo arco non orientato non pesato tra due nodi specificati.
+      /// </summary>
+      /// <param name="nodeKey1">Chiave del primo nodo.</param>
+      /// <param name="nodeKey2">Chiave del secondo nodo.</param>
+      /// <exception cref="InvalidOperationException"/>
+      public void AddUndirectEdge(TNodeKey nodeKey1, TNodeKey nodeKey2)
+      {
+         if (Equals(nodeKey1, nodeKey2))
+         {
+            throw new InvalidOperationException();
+         }
+         AddEdge(nodeKey1, nodeKey2);
+         AddEdge(nodeKey2, nodeKey1);
+      }
+
+      /// <summary>
+      /// Aggiunge un nuovo arco non orientato e pesato tra due nodi specificati.
+      /// </summary>
+      /// <param name="nodeKey1">Chiave del primo nodo.</param>
+      /// <param name="nodeKey2">Chiave del secondo nodo.</param>
+      /// <param name="edgeWeight">Peso attribuito.</param>
+      /// <exception cref="InvalidOperationException"/>
+      public void AddUndirectEdge(TNodeKey nodeKey1, TNodeKey nodeKey2, TEdgeWeight edgeWeight)
+      {
+         if (Equals(nodeKey1, nodeKey2))
+         {
+            throw new InvalidOperationException();
+         }
+         AddEdge(nodeKey1, nodeKey2, edgeWeight);
+         AddEdge(nodeKey2, nodeKey1, edgeWeight);
+      }
+
+      /// <summary>
+      /// Determina se il nodo specificato esiste nel grafo.
+      /// </summary>
+      /// <param name="nodeKey">Chiave del nodo.</param>
+      /// <returns>
+      /// True se contiene il nodo con la chiave specificata, altrimenti false.
+      ///</returns>
+      public bool ContainsNode(TNodeKey nodeKey) => _nodes.ContainsKey(nodeKey);
+
+      /// <summary>
+      /// Ritorna le chiavi dei nodi predecessori del nodo specificato.
+      /// </summary>
+      /// <param name="nodeKey">Chiave del nodo.</param>
+      /// <returns>Insieme delle chiavi dei nodi predecessori.</returns>
+      /// <exception cref="InvalidOperationException"</exception>
+      public IEnumerable<TNodeKey> GetPredecessorNodes(TNodeKey nodeKey)
+      {
+         if (!_nodes.ContainsKey(nodeKey))
+         {
+            throw new InvalidOperationException();
+         }
+
+         return _nodes
+            .SelectMany(x => x.Value.Edges.Where(y => Equals(y.DestNode.Key, nodeKey)))
+            .Distinct()
+            .Select(x => x.SourceNode.Key);
+      }
+
+      /// <summary>
+      /// Ritorna le chiavi dei nodi successori del nodo specificato.
+      /// </summary>
+      /// <param name="nodeKey">Chiave del nodo.</param>
+      /// <returns>Insieme delle chiavi dei nodi successori.</returns>
+      /// <exception cref="InvalidOperationException"</exception>
+      public IEnumerable<TNodeKey> GetSuccessorNodes(TNodeKey nodeKey)
+      {
+         if (!_nodes.ContainsKey(nodeKey))
+         {
+            throw new InvalidOperationException();
+         }
+
+         return _nodes[nodeKey].Edges.Select(x => x.DestNode.Key);
+      }
+      #endregion
+
+      #region Classes
+
+      #region GraphNode
+      protected class GraphNode
+      {
+         private readonly TNodeKey _key;
+         private readonly TNodeData _data;
+         private readonly List<GraphEdge> _edges;
+
+         #region Constructors
+         private GraphNode()
+         {
+         }
+
+         public GraphNode(TNodeKey key, TNodeData data)
+         {
+            _key = key;
+            _data = data;
+            _edges = new List<GraphEdge>();
+         }
+         #endregion
+
+         #region Public properties
+         public TNodeKey Key => _key;
+
+         public TNodeData Data => _data;
+
+         public IEnumerable<GraphEdge> Edges => _edges;
+         #endregion
+
+         #region Public methods
+         public void AddEdge(GraphNode node)
+         {
+            _edges.Add(new GraphEdge(this, node));
+         }
+
+         public void AddEdge(GraphNode node, TEdgeWeight weight)
+         {
+            _edges.Add(new GraphEdge(this, node, weight));
+         }
+         #endregion
+      }
+      #endregion
+
+      #region GraphEdge
+      protected class GraphEdge
+      {
+         private readonly GraphNode _sourceNode;
+         private readonly GraphNode _destNode;
+         private readonly TEdgeWeight _weight;
+         private readonly bool _isWeighed;
+
+         #region Constructors
+         private GraphEdge()
+         {
+         }
+
+         public GraphEdge(GraphNode sourceNode, GraphNode destNode)
+         {
+            _sourceNode = sourceNode ?? throw new ArgumentNullException("sourceNode");
+            _destNode = destNode ?? throw new ArgumentNullException("destNode");
+         }
+
+         public GraphEdge(GraphNode sourceNode, GraphNode destNode, TEdgeWeight weight)
+            : this(sourceNode, destNode)
+         {
+            //_sourceNode = sourceNode ?? throw new ArgumentNullException("sourceNode");
+            //_destNode = destNode ?? throw new ArgumentNullException("destNode");
+            _weight = weight;
+            _isWeighed = true;
+         }
+         #endregion
+
+         #region Public properties
+         public GraphNode SourceNode => _sourceNode;
+
+         public GraphNode DestNode => _destNode;
+
+         public TEdgeWeight Weight => _weight;
+
+         public bool IsWeighed => _isWeighed;
+         #endregion
+      }
+      #endregion
+
+      #endregion
+   }
+}
---
+++ CityScover.Tests/CreateGraphTest.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using CityScover.ADT.Graphs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CityScover.Tests
+{
+   [TestClass]
+   public class CreateGraphTest
+   {
+      #region Classes
+      private class InterestPoint
+      {
+         public InterestPoint(string name)
+         {
+            Name = name;
+         }
+
+         public string Name { get; set; }
+      }
+
+      private class Route : IGraphEdgeWeight
+      {
+         public Route(double distance)
+         {
+            Distance = distance;
+         }
+
+         public double Distance { get; set; }
+
+         public Func<double> Weight
+         {
+            get { return () => Distance; }
+         }
+      }
+
+      private class CityMapGraph : Graph<int, InterestPoint, Route>
+      {
+      }
+
+      #endregion
+
+      [TestMethod]
+      public void CreateGraph1()
+      {
+         CityMapGraph graph = new CityMapGraph();
+         Assert.IsTrue(graph.NodeCount == 0);
+         Assert.IsTrue(graph.EdgeCount == 0);
+
+         InterestPoint p1 = new InterestPoint("P1");
+         graph.AddNode(1, p1);
+         graph.AddNode(2, new InterestPoint("P2"));
+         graph.AddNode(3, new InterestPoint("P3"));
+         graph.AddNode(4, new InterestPoint("P4"));
+         graph.AddNode(5, new InterestPoint("P5"));
+         graph.AddNode(6, new InterestPoint("P6"));
+         Assert.IsTrue(graph.NodeCount == 6);
+
+         graph.AddEdge(1, 2, new Route(100.0));
+         graph.AddEdge(1, 4, new Route(200.0));
+         graph.AddEdge(4, 2, new Route(300.0));
+         graph.AddEdge(2, 5, new Route(400.0));
+         graph.AddEdge(5, 4, new Route(500.0));
+         graph.AddEdge(3, 5, new Route(600.0));
+         graph.AddEdge(3, 6, new Route(700.0));
+         graph.AddEdge(6, 6, new Route(0.0));
+         Assert.ThrowsException<InvalidOperationException>(() => graph.AddEdge(17, 2, new Route(250.0)));
+         Assert.ThrowsException<InvalidOperationException>(() => graph.AddEdge(1, 17, new Route(250.0)));
+         Assert.ThrowsException<InvalidOperationException>(() => graph.AddEdge(1, 2, new Route(250.0)));
+         Assert.IsTrue(graph.EdgeCount == 8);
+
+         Assert.IsTrue(graph.ContainsNode(4));
+         Assert.IsFalse(graph.ContainsNode(17));
+
+         List<int> plist = new List<int>(graph.GetPredecessorNodes(5));
+         Assert.IsTrue(plist.Count == 2);
+         Assert.IsTrue(plist[0] == 2);
+         Assert.IsTrue(plist[1] == 3);
+
+         List<int> slist = new List<int>(graph.GetSuccessorNodes(5));
+         Assert.IsTrue(slist.Count == 1);
+         Assert.IsTrue(slist[0] == 4);
+      }
+
+      [TestMethod]
+      public void CreateGraph2()
+      {
+         CityMapGraph graph = new CityMapGraph();
+         Assert.IsTrue(graph.NodeCount == 0);
+         Assert.IsTrue(graph.EdgeCount == 0);
+
+         InterestPoint p1 = new InterestPoint("P1");
+         graph.AddNode(1, p1);
+         graph.AddNode(2, new InterestPoint("P2"));
+         graph.AddNode(3, new InterestPoint("P3"));
+         graph.AddNode(4, new InterestPoint("P4"));
+         graph.AddNode(5, new InterestPoint("P5"));
+         Assert.IsTrue(graph.NodeCount == 5);
+
+         graph.AddUndirectEdge(1, 2, new Route(100.0));
+         graph.AddUndirectEdge(1, 5, new Route(200.0));
+         graph.AddUndirectEdge(2, 5, new Route(300.0));
+         graph.AddUndirectEdge(2, 4, new Route(400.0));
+         graph.AddUndirectEdge(2, 3, new Route(500.0));
+         graph.AddUndirectEdge(3, 4, new Route(600.0));
+         Assert.ThrowsException<InvalidOperationException>(() => graph.AddEdge(17, 2, new Route(250.0)));
+         Assert.ThrowsException<InvalidOperationException>(() => graph.AddEdge(1, 17, new Route(250.0)));
+         Assert.ThrowsException<InvalidOperationException>(() => graph.AddEdge(1, 2, new Route(250.0)));
+         Assert.ThrowsException<InvalidOperationException>(() => graph.AddUndirectEdge(2, 2));
+         Assert.IsTrue(graph.EdgeCount == 12);
+      }
+   }
+}
