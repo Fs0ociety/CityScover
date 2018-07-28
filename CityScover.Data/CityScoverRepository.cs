4b32d174d0b3b71ccae5ddd8230bc2c3936e7944
Andrea Ritondale <andrea.ritondale@student.unife.it>
2018-07-28 03:03:26 +0200

    Aggiunto file di dati statici XML per i punti d'interesse al progetto CityScover.Data. Creata classe CityScoverRepository.

---
+++ CityScover.Data/CityScoverRepository.cs
@@ -0,0 +1,28 @@
+﻿//
+// CityScover
+// Version 1.0
+//
+// Authors: Andrea Ritondale
+// File update: 28/07/2018
+//
+
+namespace CityScover.Data
+{
+   public static class CityScoverRepository
+   {
+      #region Constructors
+      static CityScoverRepository()
+      {
+
+      }
+      #endregion
+
+      #region Private static methods
+      // TODO
+      #endregion
+
+      #region Public static properties
+
+      #endregion
+   }
+}
