ae038843d82c9d6f5b3f2a6a6f109473f2ec4f98
thr00t <dev.andreamingard@gmail.com>
2018-10-03 11:30:00 +0200

    Inizio implementazione Lin Kernighan.

---
+++ CityScover.Engine/Algorithms/VariableDepthSearch/LinKernighanAlgorithm.cs
@@ -0,0 +1,73 @@
+﻿//
+// CityScover
+// Version 1.0
+//
+// Authors: Andrea Ritondale, Andrea Mingardo
+// File update: 03/10/2018
+//
+
+using CityScover.Engine.Workers;
+using System.Threading.Tasks;
+
+namespace CityScover.Engine.Algorithms.VariableDepthSearch
+{
+   internal class LinKernighanAlgorithm : Algorithm
+   {
+      private CityMapGraph _cityMapClone;
+      private TOSolution _bestSolution;
+      private byte _executedSteps;
+
+      #region Constructors
+      internal LinKernighanAlgorithm(byte steps)
+         : this(steps, null)
+      {         
+      }
+
+      internal LinKernighanAlgorithm(byte steps, AlgorithmTracker tracker)
+         : base(tracker)
+      {
+         MaxSteps = steps;
+      }
+      #endregion
+
+      #region Internal properties
+      internal byte MaxSteps { get; private set; } 
+      #endregion
+
+      #region Overrides
+      internal override void OnError()
+      {
+         throw new System.NotImplementedException();
+      }
+
+      internal override void OnInitializing()
+      {
+         _cityMapClone = Solver.CityMapGraph.DeepCopy();
+         // TODO: la prende dal solver?
+         _bestSolution = Solver.BestSolution;
+      }
+
+      internal override void OnTerminated()
+      {
+         throw new System.NotImplementedException();
+      }
+
+      internal override void OnTerminating()
+      {
+         // TODO: confronto finale per vedere se effettivamente ho incrementato qualcosa
+         // mi verrebbe da farlo qua.
+         throw new System.NotImplementedException();
+      }
+
+      internal override async Task PerformStep()
+      {
+         throw new System.NotImplementedException();
+      }
+
+      internal override bool StopConditions()
+      {
+         return _executedSteps == MaxSteps;
+      } 
+      #endregion
+   }
+}
